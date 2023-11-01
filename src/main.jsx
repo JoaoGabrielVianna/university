c644894dc9c9e6f45303a1199d23a0c8a4a0f888
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-11-01 10:24:45 -0300

    google funciona

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './routes/App-screen/App'
 import Start_Screen from './routes/Start-screen/Start'
 import Error_Screen from './routes/Error-screen/Error'
@@ -10,30 +10,40 @@ import Register_Screen from './routes/Register-screen/Register'
 import './index.css'
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <Error_Screen />,
-    children: [
-      {
-        path: '/', element: <Start_Screen />
-      },
-      {
-        path: '/login', element: <Login_Screen />
-      },
-      {
-        path: '/register', element: <Register_Screen />
-      },
-      {
-        path: '/home', element: <Home_Screen />
-      }
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <Error_Screen />,
+//     children: [
+//       {
+//         path: '/', element: <Start_Screen />
+//       },
+//       {
+//         path: '/login', element: <Login_Screen />
+//       },
+//       {
+//         path: '/register', element: <Register_Screen />
+//       },
+//       {
+//         path: '/home', element: <Home_Screen />
+//       }
+//     ]
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Start_Screen/>}/>
+        <Route path='/login' element={<Login_Screen/>}/>
+        <Route path='/register' element={<Register_Screen/>}/>
+        <Route path='/home' element={<Home_Screen/>}/>
+        <Route path='/*' element={<Error_Screen/>}/>
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- src/services/firebaseConfig.jsx
+++ src/services/firebaseConfig.jsx
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyBmZCBU08hho7Ef3s_7AgPU1THQerVmvjc",
+
   authDomain: "database-1de4e.firebaseapp.com",
+
   projectId: "database-1de4e",
+
   storageBucket: "database-1de4e.appspot.com",
+
   messagingSenderId: "616521156267",
+
   appId: "1:616521156267:web:a80bc06e91737638da59ee"
+
 };
+
+
 
 
 export const app = initializeApp(firebaseConfig);
