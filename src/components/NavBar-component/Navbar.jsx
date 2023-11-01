40ec4037987e9ece45a982a46f143a0ed0995807
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-11-01 16:36:38 -0300

    Corrigindo o erro no arquivo Navbar.jsx 3

--- src/components/NavBar-component/Navbar.jsx
+++ src/components/NavBar-component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./NavBar.css";
+// import "./NavBar.css";
 import { Link } from "react-router-dom";
 import Faculdade_Icon from "./icons-component/faculdade-icon";
 import Destaques_Icon from "./icons-component/destaques-icon";
