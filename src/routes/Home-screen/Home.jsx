b928f38b0b1909ef07c443d547512e1af33376b8
JoaoGabrielVianna <joaogabrielvianna05@gmail.com>
2023-11-01 22:04:52 -0300

    iniciando a tela Home

--- src/routes/Home-screen/Home.jsx
+++ src/routes/Home-screen/Home.jsx
@@ -1,20 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import './Home.css'
 import { AuthGoogleContext } from "../../contexts/authGoogle";
-import { Link } from "react-router-dom";
-import NavBar from "../../components/NavBar-component/Navbar";
+import search_icon from "../../assets/svgs/search-icon.svg";
+import arrow_icon from "../../assets/svgs/arrow-icon.svg";
+import notification_icon from "../../assets/svgs/notification-icon.svg";
 
-export default function Home_Screen(){
+export default function Home_Screen() {
+    const [showfaculdades, setShowfaculdades] = useState(false)
     const { user, signOut } = useContext(AuthGoogleContext)
     const userLogado = JSON.parse(user)
-    return(
+    return (
         <>
             <main id="main_home">
-            <h1>TELA HOME</h1>
-        
-            
+                <header>
+                    <div className="div1">
+                        <div id="searchBar">
+                            <span><img src={search_icon} alt="" /></span>
+
+                            <input placeholder="Pesquisar" />
+                        </div>
+                        <span><img src={notification_icon} alt="" /></span>
+                    </div>
+
+                    <div className="faculdades">
+                        <span className="header">
+                            <h1>Faculdades</h1>
+                            <div onClick={() => setShowfaculdades(!showfaculdades)}>
+                                <img src={arrow_icon} alt="" />
+                            </div>
+
+                        </span>
+                        <span className="body" style={{height: showfaculdades ? "150px" : "0"}}>
+
+                        </span>
+
+                    </div>
+                </header>
+                <h1>TELA HOME</h1>
+
+
             </main>
-            
+
         </>
     )
 }
